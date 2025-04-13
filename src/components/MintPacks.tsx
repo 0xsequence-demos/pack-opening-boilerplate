f3372a6d1820b9a388299a74e195f5b56d2d9d4e
Tomasz Dysinski <dysinski.tomasz@gmail.com>
2025-04-13 01:01:05 -0700

    added minting

---
+++ src/components/MintPacks.tsx
@@ -0,0 +1,69 @@
+import { Spinner } from "@0xsequence/design-system";
+import { useEffect, useState } from "react";
+import { useAccount, useSignMessage } from "wagmi";
+import { Button } from "boilerplate-design-system";
+
+export default function MintPacks(props: {
+  refetchPackCollection: () => void;
+}) {
+  const { address } = useAccount();
+  const {
+    isPending: isEncoding,
+    data: signedMessage,
+    signMessage,
+  } = useSignMessage();
+
+  useEffect(() => {
+    if (address && !signedMessage && !isEncoding) {
+      signMessage({ message: "let me in" });
+    }
+  }, [address, isEncoding, signMessage, signedMessage]);
+
+  const [isMinting, setIsMinting] = useState(false);
+
+  const requestMint = () => {
+    console.log("WWEW");
+    if (isMinting) {
+      return;
+    }
+    setIsMinting(true);
+    fetch("api/mint", {
+      method: "POST",
+      headers: {
+        authorization: `${address}:${signedMessage}`,
+      },
+      body: JSON.stringify({
+        address,
+        tokenId: "1",
+        amount: 3,
+      }),
+    })
+      .then((response) => {
+        response.json().then((d) => {
+          console.log(d);
+          setTimeout(() => {
+            setIsMinting(false);
+            props.refetchPackCollection();
+          }, 1000);
+        });
+      })
+      .catch((e) => {
+        console.log(e);
+        setTimeout(() => {
+          setIsMinting(false);
+        }, 5000);
+      });
+  };
+
+  return (
+    <Button
+      variant="secondary"
+      className="purchase"
+      onClick={requestMint}
+      type="button"
+      disabled={isEncoding || isMinting}
+    >
+      {isEncoding || isMinting ? <Spinner /> : `Mint 3 Packs`}
+    </Button>
+  );
+}
--- src/views/Connected.tsx
+++ src/views/Connected.tsx
@@ -10,6 +10,7 @@ import View3D from "../components/3d/View3D";
 import ItemViewer3D from "../components/3d/ItemViewer3D";
 import GenericItem from "../components/3d/GenericItem";
 import { useCollectionBalance } from "../hooks/data";
+import MintPacks from "../components/MintPacks";
 
 const Connected = (props: { userAddress: Address; chainId: number }) => {
   const { userAddress, chainId } = props;
@@ -30,7 +31,10 @@ const Connected = (props: { userAddress: Address; chainId: number }) => {
     openPack,
   } = useOpenPack({ address: userAddress });
 
-  const { data: packCollectionBalanceData } = useCollectionBalance({
+  const {
+    data: packCollectionBalanceData,
+    refetch: refetchPackCollectionBalance,
+  } = useCollectionBalance({
     accountAddress: userAddress,
     contractAddress: packContractAddress,
   });
@@ -41,8 +45,9 @@ const Connected = (props: { userAddress: Address; chainId: number }) => {
 
   const packModelUri = packChest?.tokenMetadata?.animation_url;
 
-  console.log(packChest);
-
+  const count = packCollectionBalanceData
+    ? packCollectionBalanceData.reduce((pv, cv) => Number(cv.balance) + pv, 0)
+    : "";
   return (
     <div className="flex flex-col gap-12">
       <View3D>
@@ -53,17 +58,20 @@ const Connected = (props: { userAddress: Address; chainId: number }) => {
         </ItemViewer3D>
       </View3D>
       <Group title="Pack Opening">
-        <Card className="flex flex-col gap-5 bg-white/10 border border-white/10 backdrop-blur-sm text-center p-0">
-          <UserInventory
-            userAddress={userAddress}
-            chainId={chainId}
-            contractAddress={packContractAddress}
-            title={"Packs"}
+        {count === 0 ? (
+          <MintPacks
+            refetchPackCollection={() => refetchPackCollectionBalance()}
           />
-        </Card>
-        <Button variant="primary" onClick={() => openPack()}>
-          Open a Pack
-        </Button>
+        ) : (
+          <>
+            <Card className="flex flex-col gap-5 bg-white/10 border border-white/10 backdrop-blur-sm text-center p-0">
+              You own {count} pack{count === 1 ? "" : "s"}
+            </Card>
+            <Button variant="primary" onClick={() => openPack()}>
+              Open a Pack
+            </Button>
+          </>
+        )}
         <p>isLoading: {isLoading ? "yes" : "..."}</p>
         <p>isError: {isError ? "yes" : "..."}</p>
         <div>
